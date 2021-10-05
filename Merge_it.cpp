39d3fe22cebf34c3b86e5f505db5f00ffd471b63
Arpit Das <47268523+DecoderRony@users.noreply.github.com>
2021-10-05 22:36:29 +0530

    Update Merge_it.cpp

--- Merge_it.cpp
+++ Merge_it.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // Second subarray is arr[mid+1..end]
 void merge(int array[], int const left, int const mid, int const right)
 {
-	auto const subArrayOne = mid - left ;
+	auto const subArrayOne = mid - left + 1 ;
 	auto const subArrayTwo = right - mid;
 
 	// Create temp arrays
